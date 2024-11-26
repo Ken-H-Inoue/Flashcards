152e1b9af65ed10d2b29b409e442b738ac86a441
Ken Hiroshi Inoue <kenhinoue@gmail.com>
2024-11-26 14:57:19 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o clima do sertão nordestino?',
+    'Clima sedmiárido'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz aeroporto em Inglês?',
+    'Aeroporto em ingles é Airport (ÉIRPORTI)'
+)
